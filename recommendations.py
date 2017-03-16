9f6637363fc22396312f2ad86382803e29f44d14
Caleb Sandfort <csandfort@isupport.com>
2017-03-16 16:43:11 -0700

    Examples

--- recommendations.py
+++ recommendations.py
@@ -168,6 +168,21 @@ def getRecommendedItems(prefs, itemMatch, user):
     return rankings
 
 
+def readfile(filename):
+    lines = [line for line in open(filename)]
+
+    # First line is the column titles
+    colnames = lines[0].strip().split(',')[1:]
+    rownames = []
+    data = []
+    for line in lines[1:]:
+        p = line.strip().split(',')
+        # First column in each row is the rowname
+        rownames.append(p[0])
+        # The data for this row is the remainder of the row
+        data.append([float(x) for x in p[1:]])
+    return rownames, colnames, data
+
 def loadMovieLens(path='/data/movielens'):
     # Get movie titles
     movies = {}
